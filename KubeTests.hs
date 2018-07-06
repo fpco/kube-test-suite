9e3b0f88a9e0fb68f7a25d9149417f12833823e1
Mike McGirr <mike@fpcomplete.com>
2018-07-06 13:47:08 -0700

    Refactor tests to use better practices from RIO

--- KubeTests.hs
+++ KubeTests.hs
@@ -2,24 +2,25 @@
 {- stack
    script
    --resolver lts-11.15
+   --package rio
    --package turtle
    --package aeson
    --package hspec
-   --package text
 -}
 
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE DeriveGeneric     #-}
 
 -- Swap `script` with `exec ghci` to load script into ghci REPL
 -- If on NixOS add --nix and --no-nix-pure
 
-import Turtle
+import RIO
+import RIO.Text (encodeUtf8)
+import RIO.ByteString.Lazy (fromStrict)
+import Turtle (shellStrict, empty, need, fromText)
 import Data.Aeson
-import Data.Text.Lazy (fromStrict)
-import qualified Data.Text.Lazy as TL
-import Data.Text.Lazy.Encoding (encodeUtf8)
 import GHC.Generics (Generic)
 import Test.Hspec
 
@@ -68,21 +69,21 @@ instance FromJSON Service where
     clusterIP <- spec .: "clusterIP"
     return Service{..}
 
-getNodes :: IO ()
+getNodes :: (MonadIO m, MonadReader env m, HasLogFunc env, HasCallStack) => m ()
 getNodes = do
   (_exitCode, kubeGetNodesText) <- shellStrict "kubectl -o=json get nodes" empty
-  let kubeGetNodesByteString = encodeUtf8 $ fromStrict $ kubeGetNodesText
+  let kubeGetNodesByteString = fromStrict $ encodeUtf8 $ kubeGetNodesText
       parsedNodes :: Either String Nodes
       parsedNodes = eitherDecode kubeGetNodesByteString
   case parsedNodes of
-    Left e -> putStrLn $ "There was an error attempting to parse" <> e
+    Left e -> logError $ "There was an error attempting to parse" <> fromString e
     Right nodes -> do
-      putStrLn "Success"
-      putStrLn $
+      logInfo "Success"
+      logInfo $
         "Found the following nodes on a cluster running api version: " <>
-          (show $ apiVersion nodes)
-      mapM_ (putStrLn . show . nodeName) (itemsNodes nodes)
-  hspec $ do
+          (displayShow $ apiVersion nodes)
+      mapM_ (logInfo . displayShow . nodeName) (itemsNodes nodes)
+  liftIO $ hspec $ do
     describe "Test to list the names of nodes in the kubernetes cluster" $ do
       it "The list of names of the nodes in the cluster is not empty" $ do
         case parsedNodes of
@@ -91,26 +92,26 @@ getNodes = do
               `shouldSatisfy`
               (\i -> (not $ null i))
 
-getServices :: IO ()
+getServices :: (MonadIO m, MonadReader env m, HasLogFunc env, HasCallStack) => m ()
 getServices = do
   (_exitCode, kubeGetServicesText) <- shellStrict "kubectl -o=json get services" empty
-  let kubeGetServicesByteString = encodeUtf8 $ fromStrict $ kubeGetServicesText
+  let kubeGetServicesByteString = fromStrict $ encodeUtf8 $ kubeGetServicesText
       parsedServices :: Either String Services
       parsedServices = eitherDecode kubeGetServicesByteString
   case parsedServices of
     Left e ->
-      putStrLn $ "Error while parsing JSON returned from `get services`.\
-                 \ There was an error attempting to parse" <> e
+      logError $ "Error while parsing JSON returned from `get services`.\
+                 \ There was an error attempting to parse" <> fromString e
     Right services -> do
-      putStrLn "Success. Found services on the target cluster"
-      putStrLn $ "\nFound the following service names:"
-      mapM_ (putStrLn . show . serviceName) (itemsServices services)
-      putStrLn $ "\nFound the following service creation timestamps:"
-      mapM_ (putStrLn . show . creationTimestamp) (itemsServices services)
-      putStrLn $ "\nFound the following cluster IP addresses for the services:"
-      mapM_ (putStrLn . show . clusterIP) (itemsServices services)
-      -- Eventually we will reorganize the tests and following Hspec reporting
-      hspec $ do
+      logInfo "Success. Found services on the target cluster"
+      logInfo $ "\nFound the following service names:"
+      mapM_ (logInfo . displayShow . serviceName) (itemsServices services)
+      logInfo $ "\nFound the following service creation timestamps:"
+      mapM_ (logInfo . displayShow . creationTimestamp) (itemsServices services)
+      logInfo $ "\nFound the following cluster IP addresses for the services:"
+      mapM_ (logInfo . displayShow . clusterIP) (itemsServices services)
+      -- Eventually we will reorganize the above reporting and following Hspec tests
+      liftIO $ hspec $ do
         describe "Test the services in the Kubernetes cluster" $ do
           it "The list of names of services in the cluster is not empty" $ do
             (map serviceName (itemsServices services))
@@ -121,27 +122,22 @@ getServices = do
               `shouldSatisfy`
               (not . null)
 
--- TODO
--- getPods :: IO ()
--- getPods = undefined
-
 main = do
   clusterName <- need "CLUSTER_NAME"
   kubeConfig  <- need "KUBECONFIG"
 
-  -- TODO
-  -- Eventually allow KUBECONFIG and/or CLUSTER_NAME to be set interactively
-  -- if they are not set in the env.
-
-  case (clusterName, kubeConfig) of
-    (Nothing, Nothing) ->
-      putStrLn "Missing CLUSTER_NAME and KUBECONFIG environmental variables."
-    (Just _, Nothing) ->
-      putStrLn "Missing KUBECONFIG environmental variable."
-    (Nothing, Just _) ->
-      putStrLn "Missing KUBECONFIG environmental variable."
-    (Just _, Just _) -> do
-      putStrLn "Running tests on nodes.\n"
-      getNodes
-      putStrLn "\nNow running tests on services.\n"
-      getServices
+  lo <- logOptionsHandle stdout False
+  withLogFunc lo $ \logFn -> do
+    runRIO logFn $ do
+      case (clusterName, kubeConfig) of
+        (Nothing, Nothing) ->
+          logError "Missing CLUSTER_NAME and KUBECONFIG environmental variables."
+        (Just _, Nothing) ->
+          logError "Missing KUBECONFIG environmental variable."
+        (Nothing, Just _) ->
+          logError "Missing KUBECONFIG environmental variable."
+        (Just _, Just _) -> do
+          logInfo "Running tests on nodes.\n"
+          getNodes
+          logInfo "\nNow running tests on services.\n"
+          getServices
